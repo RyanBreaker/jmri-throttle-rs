cd695cf22c746dd0dc0f1b5517de840c08b046c1
Ryan Breaker <ryan@breaker.rocks>
2023-11-25 10:49:15 -0600

    Add async function for JMRI server connection and error handling

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 mod message;
 
 use crate::message::WiMessage;
-use futures::StreamExt;
+use futures::{SinkExt, StreamExt};
 use log::Level::Debug;
-use log::{debug, error, log_enabled};
+use log::{debug, error, info, log_enabled};
 use std::collections::HashMap;
+use std::error::Error;
 use std::sync::Arc;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
-use tokio::sync::{mpsc, RwLock};
+use tokio::sync::mpsc::UnboundedSender;
+use tokio::sync::{mpsc, Notify, RwLock};
 use tokio_stream::wrappers::UnboundedReceiverStream;
+use tokio_util::codec::{Framed, LinesCodec};
 use uuid::Uuid;
 use warp::http::StatusCode;
 use warp::ws::{Message, WebSocket};
@@ -22,30 +24,76 @@ pub struct Client {
 type Clients = Arc<RwLock<HashMap<Uuid, mpsc::UnboundedSender<Message>>>>;
 
 const SERVER: &str = "localhost:12090";
+const THROTTLE_NAME: &str = "TestThrottleRs";
 
-#[tokio::main]
-async fn main() -> std::io::Result<()> {
-    pretty_env_logger::init();
-
+async fn jmri_conn(
+    notify: Arc<Notify>,
+    tx: UnboundedSender<String>,
+    mut rx: UnboundedReceiverStream<String>,
+) -> Result<(), Box<dyn Error>> {
     let my_id = Uuid::new_v4();
     debug!("Server's ID: {my_id}");
 
-    let (jmri_tx, jmri_rx) = mpsc::unbounded_channel::<String>();
-    let mut jmri_rx = UnboundedReceiverStream::new(jmri_rx);
-
-    let mut jmri_conn = TcpStream::connect(SERVER).await.unwrap_or_else(|e| {
+    let jmri_conn = TcpStream::connect(SERVER).await.unwrap_or_else(|e| {
         error!("Error connecting to JMRI at '{SERVER}': {e}");
-        panic!();
+        std::process::exit(1);
+    });
+    let (mut jmri_tx, mut jmri_rx) = Framed::new(jmri_conn, LinesCodec::new()).split::<String>();
+
+    // Notify we're connected and main init can continue
+    info!("Successfully connected to JMRI at: {SERVER}");
+    notify.notify_one();
+
+    let read_handle = tokio::spawn(async move {
+        while let Some(line) = jmri_rx.next().await {
+            let line = match line {
+                Ok(line) => line,
+                Err(e) => {
+                    error!("Error reading from JMRI: {e}");
+                    break;
+                }
+            };
+            if let Err(e) = tx.send(line) {
+                error!("Error sending message from JMRI: {e}");
+            }
+        }
     });
 
-    // TODO: set up async task for sending and receiving messages from JMRI
-    jmri_conn.write_all(b"HU{}").await?;
+    // Setup message to JMRI
+    jmri_tx
+        .send(format!("HU{my_id}\r\nN{THROTTLE_NAME}\r\n"))
+        .await
+        .unwrap();
 
-    let mut buffer = [0; 1024];
-    let n = jmri_conn.read(&mut buffer).await?;
-    if n > 0 {
-        debug!("Received: {}", String::from_utf8_lossy(&buffer));
-    }
+    let write_handle = tokio::spawn(async move {
+        while let Some(line) = rx.next().await {
+            if let Err(e) = jmri_tx.send(line).await {
+                error!("Error sending message to JMRI: {e}");
+            }
+        }
+    });
+
+    let _ = futures::future::join(read_handle, write_handle).await;
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    pretty_env_logger::init();
+
+    let (jmri_tx, jmri_rx) = mpsc::unbounded_channel::<String>();
+    let jmri_rx = UnboundedReceiverStream::new(jmri_rx);
+
+    let jmri_notify = Arc::new(Notify::new());
+    let jmri_up2 = jmri_notify.clone();
+    let _jmri_handle = tokio::spawn(async move {
+        if let Err(e) = jmri_conn(jmri_up2, jmri_tx, jmri_rx).await {
+            error!("Error on jmri_conn: {e}");
+        }
+    });
+    // Lets us know we're connected to JMRI and can continue
+    jmri_notify.notified().await;
 
     let health = warp::path!("health")
         .and(warp::get())
@@ -64,9 +112,7 @@ async fn main() -> std::io::Result<()> {
 
     let routes = health.or(ws);
 
-    warp::serve(health.or(routes))
-        .run(([0, 0, 0, 0], 5000))
-        .await;
+    warp::serve(routes).run(([0, 0, 0, 0], 5000)).await;
 
     Ok(())
 }
@@ -75,9 +121,9 @@ async fn connected(ws: WebSocket, clients: Clients) {
     let id = Uuid::new_v4();
     debug!("New id: {id}");
 
-    let (mut ws_tx, mut ws_rx) = ws.split();
-    let (tx, rx) = mpsc::unbounded_channel::<Message>();
-    let mut rx = UnboundedReceiverStream::new(rx);
+    let (_ws_tx, mut ws_rx) = ws.split();
+    let (tx, _rx) = mpsc::unbounded_channel::<Message>();
+    // let mut rx = UnboundedReceiverStream::new(rx);
 
     clients.write().await.insert(id, tx);
     if log_enabled!(Debug) {
