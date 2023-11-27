8de4585028547342eaac0eb72f942cc9d188ce72
Ryan Breaker <ryan@breaker.rocks>
2023-11-26 18:06:22 -0600

    Refactor message definitions in message.rs
    The commit refactors the `WiMessageType` enum, adding new types and renaming existing ones to increase clarity. The `Direction` and `WiMessageType` are now introduced with display functions. The commit also introduces the `Address`, `Velocity`, `Function` type aliases for better readability, and a new Display implementation for `WiMessage` for more comfortable string conversion.

--- src/message.rs
+++ src/message.rs
@@ -1,22 +1,66 @@
 use serde::{Deserialize, Serialize};
+use std::fmt::{Display, Formatter};
 
-#[derive(Serialize, Deserialize, Debug)]
+pub type Address = i16;
+pub type Velocity = i16;
+pub type Function = u8;
+
+#[derive(Serialize, Deserialize, Debug, Copy, Clone)]
 pub enum Direction {
     Reverse = 0,
     Forward = 1,
 }
 
+impl Display for Direction {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&format!("R{}", *self as u8))
+    }
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 pub enum WiMessageType {
-    AddEngine(String),
-    RemoveEngine(String),
-    Throttle(usize),
-    Function(usize),
+    AddAddress,
+    RemoveAddress,
+    Velocity(Velocity),
+    FunctionPressed(Function),
+    FunctionReleased(Function),
     Direction(Direction),
+}
+
+impl WiMessageType {
+    pub fn is_address(&self) -> bool {
+        matches!(self, Self::AddAddress | Self::RemoveAddress)
+    }
+}
+
+impl Display for WiMessageType {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let s = match self {
+            WiMessageType::Velocity(throttle) => format!("V{throttle}"),
+            WiMessageType::FunctionPressed(func) => format!("F1{func}"),
+            WiMessageType::FunctionReleased(func) => format!("F0{func}"),
+            WiMessageType::Direction(dir) => dir.to_string(),
+            _ => String::new(),
+        };
+
+        f.write_str(&s)
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct WiMessage {
     message_type: WiMessageType,
-    address: u16,
+    address: Address,
+}
+
+impl Display for WiMessage {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let s = if self.message_type.is_address() {
+            format!("MT{}L{}<;>", self.message_type, self.address)
+        } else {
+            format!("MTAL{}<;>{}", self.address, self.message_type)
+        };
+
+        f.write_str(&s)
+    }
 }
